b3e892ea788915719a6d85f75d2570582a92c22a
Aderemi Abiodun <aderemibiodun1000@gmail.com>
2025-08-08 18:28:39 +0100

    modify custom input class

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:note_app_flutter/screens/create_note.dart";
+// import "package:note_app_flutter/screens/create_note.dart";
 import "package:note_app_flutter/utilis/custom_input.dart";
 
 class HomeScreen extends StatelessWidget {
@@ -8,20 +8,37 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text("RemsNotePad"),
-          const SizedBox(height: 20),
-          // Search Box
-          CustomInput(),
-          const SizedBox(height: 20),
-          SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            padding: EdgeInsets.all(12),
-            child: CreateNoteScreen(),
-          ),
-        ],
+      // appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text("RemsNotePad"),
+            const SizedBox(height: 20),
+            // Search Box
+            CustomInput(
+              inputHintText: "Search notes",
+              inputPrefixIcon: Icon(Icons.search),
+              inputTextController: TextEditingController(),
+            ),
+            const SizedBox(height: 20),
+
+            CustomInput(
+              inputHintText: "Title",
+              inputPrefixIcon: Icon(Icons.keyboard),
+              inputTextController: TextEditingController(),
+              inputKeyboardType: TextInputType.multiline,
+            ),
+            const SizedBox(height: 20),
+
+            CustomInput(
+              inputHintText: "Write your note here",
+              inputPrefixIcon: Icon(Icons.note_add),
+              inputTextController: TextEditingController(),
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
--- lib/utilis/custom_input.dart
+++ lib/utilis/custom_input.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
 class CustomInput extends StatelessWidget {
-  const CustomInput({super.key});
+  final String inputHintText;
+  final Widget? inputPrefixIcon;
+  final Widget? inputSuffixIcon;
+  final TextEditingController inputTextController;
+  final TextInputType? inputKeyboardType;
+
+  const CustomInput({
+    super.key,
+    required this.inputHintText,
+    this.inputPrefixIcon,
+    this.inputSuffixIcon,
+    this.inputKeyboardType,
+    required this.inputTextController,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
+      keyboardType: inputKeyboardType,
+      controller: inputTextController,
       decoration: InputDecoration(
-        hintText: "Search notes",
-        prefixIcon: Icon(Icons.search_rounded),
-
-        // border: InputBorder,
+        hintText: inputHintText,
+        prefixIcon: inputPrefixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
       ),
     );
   }
